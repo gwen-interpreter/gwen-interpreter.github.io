f995b9f1dfe24f9c6e17baef6e2829b813e32b11
Branko Juric <branko.juric@gmail.com>
2021-10-13 23:56:43 +1100

    misc updates

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -9,7 +9,7 @@ const FeatureList = [
     Svg: require('../../static/img/gwen-feature.svg').default,
     description: (
       <>
-        Write <Link to="https://cucumber.io/docs/gherkin/reference">feature specs</Link> to communicate scenarios and examples.
+        Write <Link to="https://cucumber.io/docs/gherkin/reference">feature specs</Link> to describe how your system should behave.
       </>
     ),
   },
@@ -18,7 +18,7 @@ const FeatureList = [
     Svg: require('../../static/img/gwen-meta.svg').default,
     description: (
       <>
-        Write <Link to="/docs/meta-specs">meta specs</Link> to describe what your features will execute.
+        Write <Link to="/docs/meta-specs">meta specs</Link> to describe what your feature specs will execute.
       </>
     ),
   },
@@ -27,7 +27,7 @@ const FeatureList = [
     Svg: require('../../static/img/gwen-portrait.svg').default,
     description: (
       <>
-        Call Gwen to bind your meta and execute your features to automate.
+        Run Gwen to bind your meta and execute your features to automate.
       </>
     ),
   },
