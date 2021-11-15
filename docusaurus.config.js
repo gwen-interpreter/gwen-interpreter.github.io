a05975e38d27928fe21ae62509b0d7cd4d2de497
Branko Juric <branko.juric@gmail.com>
2021-11-15 11:29:15 +1100

    update home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Gwen',
-  tagline: 'Automation Bindings for Gherkin',
+  tagline: 'Behavior Driven Automation',
   url: 'https://gweninterpreter.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
