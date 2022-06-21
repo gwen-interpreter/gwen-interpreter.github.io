d9da93beed38d633a29835da1acc9360cdc9d87e
Branko Juric <branko.juric@gmail.com>
2022-06-22 00:18:05 +1000

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
           label: 'Docs',
         },
         {
-          href: '/docs/reports/console',
+          href: '/docs/cli#init-options',
           position: 'left',
           label: "What's new?",
         },
