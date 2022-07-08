a86af29d5ff6b0bf0d33720b26146b16924621c5
Branko Juric <branko.juric@gmail.com>
2022-07-08 11:33:08 +1000

    restore bannder button styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button banner-button button--lg"
+            className="button button--secondary button--lg"
             to="/docs/get-started">
             Get Started
           </Link>
