ba39a4e6bf88e3d3e7cb1adb0156a9e3a64bd7ae
Branko Juric <branko.juric@gmail.com>
2022-07-10 09:30:51 +1000

    update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Gwen',
-  tagline: 'Automate web testing and robotic processing',
+  tagline: 'Web testing and robotics for Gherkin',
   url: 'https://gweninterpreter.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
