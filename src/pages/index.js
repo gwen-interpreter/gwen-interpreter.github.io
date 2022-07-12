1e0aa76a4dafafd2bab1ce626c8ca5ddf4fe9620
Branko Juric <branko.juric@gmail.com>
2022-07-12 13:01:03 +1000

    tagline and banner image update

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
       </Head>
       <div className="container">
         <h1 className="hero__title">
-          <span className="highlight">Web automation</span> and robotics for Gherkin
+          <span className="highlight">Web automation and robotics</span> for Gherkin
         </h1>
         <br></br>
         <div className={styles.buttons}>
