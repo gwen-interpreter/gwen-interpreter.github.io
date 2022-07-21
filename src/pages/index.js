af17b166cc1fd56813da245ea64ac7c0b4431d66
Branko Juric <branko.juric@gmail.com>
2022-07-21 13:38:59 +1000

    update subtitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <h1 className="hero__title">
           <span className="highlight">Declarative</span> automation and robotics
         </h1>
-        <p className="hero__subtitle">for Gherkin</p>
+        <p className="hero__subtitle">Executable Gherkin</p>
         <div className={styles.buttons}>
           <Link
             className="button banner-button button--lg"
