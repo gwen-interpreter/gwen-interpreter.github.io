7066c7c30b10a78107c435c5abd5f6cd42522d45
Branko Juric <branko.juric@gmail.com>
2022-07-22 16:24:47 +1000

    tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Gwen',
-  tagline: 'A readily executable Gherkin language for web automatioan and robotics',
+  tagline: 'A readily executable Gherkin language for web automation and robotics',
   url: 'https://gweninterpreter.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
