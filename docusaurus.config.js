b7136eac6ca822af1e29959e2d6037e05b75c3f2
Branko Juric <branko.juric@gmail.com>
2022-07-23 12:49:43 +1000

    tagline and size

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'Gwen web automation and robotics',
-  tagline: 'Meta programming for Gherkin',
+  tagline: 'Meta programmable Gherkin',
   url: 'https://gweninterpreter.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
