7593c05925a5c02f2eeccd9e57fc753a72cbbfd4
Branko Juric <branko.juric@gmail.com>
2022-10-01 20:29:44 +1000

    remove blurb heading from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,16 +49,15 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col col--12">
-                <h3>Behaviour Driven</h3>
-                Gwen helps you automate acceptance testing and robotic processing with behaviour described in feature specifications.
-                An embedded web engine executes each step in every scenario according to your meta to perform operations in browsers for you. Your features execute unchanged and unconcerned with automation details.
-                <div className={styles.buttons} style={{'padding-top': '20px', 'padding-bottom': '37px'}}>
-                  <Link
-                    className="button banner-button button--lg"
-                    to="/docs/introduction">
-                    Learn More
-                  </Link>
-                </div>
+              Gwen helps you automate acceptance testing and robotic processing with behaviour described in feature specifications.
+              An embedded web engine executes each step in every scenario according to your meta to perform operations in browsers for you. Your features execute unchanged and unconcerned with automation details.
+              <div className={styles.buttons} style={{'padding-top': '20px', 'padding-bottom': '37px'}}>
+                <Link
+                  className="button banner-button button--lg"
+                  to="/docs/introduction">
+                  Learn More
+                </Link>
+              </div>
             </div>
           </div>
         </div>
