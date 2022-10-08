9feff13a090f9558e895194cac0717a3cc038f2f
Branko Juric <branko.juric@gmail.com>
2022-10-08 14:02:53 +1100

    update subtitle

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ module.exports = {
   projectName: 'gwen-interpreter.github.io', // Usually your repo name.
   trailingSlash: false,
   customFields: {
-    description: 'Automate end-to-end testing and robotic processing with the Gherkin language',
+    description: 'Automate E2E testing and robotic processing with the Gherkin language',
   },
   themeConfig: {
     image: 'img/gwen-logo.png',
