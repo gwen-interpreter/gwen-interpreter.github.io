fdcdb3a2b1730a7674cd3f10a486b27537a7c03b
Branko Juric <branko.juric@gmail.com>
2023-01-25 07:55:07 +1100

    update sponsor button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ module.exports = {
         {
           type: 'html',
           position: 'right',
-          value: '<div class="sponsor"><iframe src="https://github.com/sponsors/gwen-interpreter/button" title="Sponsor gwen-interpreter" height="32" width="114" style="border: 0; border-radius: 6.5px; overflow: hidden;"></iframe></div>',
+          value: '<iframe src="https://github.com/sponsors/gwen-interpreter/button" title="Sponsor gwen-interpreter" height="32" width="114" style="border: 0; border-radius: 6px; margin-top: 6px;"></iframe>',
         },
       ],
     },
