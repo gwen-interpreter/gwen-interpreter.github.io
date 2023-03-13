ab7e832ad9cb41c295d446acd4c400976101c550
Branko Juric <branko.juric@gmail.com>
2023-03-14 10:26:46 +1100

    home page wording

--- src/pages/index.js
+++ src/pages/index.js
@@ -130,7 +130,7 @@ export default function Home() {
             </div>
             <div className="col col--6">
             <h2>Launch Gwen</h2>
-              <p>Launch Gwen to execute your features according to your meta.</p>
+              <p>Launch Gwen to execute your features.</p>
               <p>
                   <Tabs
                     groupId="jstool"
