826086976448aaa3fd68db16bd084ab11619c03f
Branko Juric <branko.juric@gmail.com>
2024-04-24 11:22:52 +1000

    Change favicon and fix sponsor link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,9 +66,9 @@ module.exports = {
           position: 'right'
         },
         {
-          type: 'html',
-          position: 'right',
-          value: '<div class="sponsor"><iframe src="https://github.com/sponsors/gwen-interpreter/button" title="Sponsor gwen-interpreter" height="32" width="114" style="border: 0; border-radius: 6px; overflow: hidden;"></iframe></div>',
+          label: '♡ Sponsor',
+          href: 'https://github.com/sponsors/gwen-interpreter',
+          position: 'right'
         },
       ],
     },
